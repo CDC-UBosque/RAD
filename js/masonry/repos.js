7228021b44131ba95c88c5413312f5a014aafe93
Unknown <seguragabriel@unbosque.edu.co>
2019-02-19 08:53:35 -0500

    sincronizacion RAD

--- js/masonry/repos.js
+++ js/masonry/repos.js
@@ -1,59 +1,60 @@
-//(function(w) {
-//  //"use strict";
-//  //let applyBtn = document.getElementById("apply");
-//  // if (window.matchMedia("(max-width: 700px)").matches) {
-//  //   console.log("uyeyeyeyey");
-//  // }
-//  function onResize() {
-//    let msGrid = document.querySelector(".paginas-tabs .blocktabs #blocktabs-asociacion_gestion_tabs-2 .view-content"),
-//      gridItems = document.querySelectorAll(".paginas-tabs .blocktabs #blocktabs-asociacion_gestion_tabs-2 .view-content .views-row"),
-//      gridColsInput = 3,
-//      gridCols = gridColsInput,
-//      gutter = 32, //Vertical and Horizontal Gutter (space)
-//      gridWidth = msGrid.offsetWidth - (gridCols - 1) * gutter,
-//      colWidth = gridWidth / gridCols;
-//
-//    for (let i = 0; i < gridItems.length; i++) {
-//      //bas style for all
-//      gridItems[i].style.position = "absolute";
-//      gridItems[i].style.maxWidth = colWidth + "px";
-//
-//      // Left position
-//      gridItems[i].style.left = (i % gridCols) * (colWidth + gutter) + "px";
-//
-//      if (i < gridCols) {
-//        gridItems[i].style.top = "0px";
-//      } else {
-//        const offsetTop = gridItems[i - gridCols].offsetTop,
-//          offsetHeight = gridItems[i - gridCols].offsetHeight;
-//        gridItems[i].style.top = offsetHeight + gutter + offsetTop + "px";
-//      }
-//    }
-//  }
-//
-//  document.addEventListener("DOMContentLoaded", onResize);
-//  window.addEventListener("resize", onResize);
-//  //applyBtn.addEventListener("click", onResize);
-//})(window);
+(function( $ ) {
+$('.path-frontpage .region-content-home .galeria-asi-lo-vivimos-home>div .view-content .views-row .views-field-field-imagenes-de-asi-lo-vivimos .jcarousel-wrapper .jcarousel')
+    .on('jcarousel:create jcarousel:reload', function() {
+        var element = $(this),
+            width = element.innerWidth();
 
+        if (width > 951) {
+            width = width / 3;
+        } else if (width < 950) {
+            width = width / 2;
+        }
 
+        element.jcarousel('items').css('width', width + 'px');
+    })
+    //.jcarousel({
+        // Your configurations options
+    //});
+})(jQuery);
 
-//(function( $ ) {
-//	var macy = Macy({
-//	    container: '.paginas-tabs .blocktabs #blocktabs-asociacion_gestion_tabs-2 .view-content',
-//	    trueOrder: false,
-//	    waitForImages: false,
-//	    margin: 24,
-//	    columns: 3,
-//	    breakAt: {
-//	        1200: 3,
-//	        940: 3,
-//	        520: 2,
-//	        400: 1
-//	    }
-//	});
-//})(jQuery);
+(function( $ ) {
+$('.path-frontpage .region-content-home .galeria-proyectos-home .view-galerias .view-content .jcarousel-wrapper .jcarousel')
+    .on('jcarousel:create jcarousel:reload', function() {
+        var element = $(this),
+            width = element.innerWidth();
 
+        if (width > 951) {
+            width = width / 3;
+        } else if (width < 950) {
+            width = width / 2;
+        }
+
+        element.jcarousel('items').css('width', width + 'px');
+    })
+    //.jcarousel({
+        // Your configurations options
+    //});
+})(jQuery);
+
+(function( $ ) {
+$('.page-node-type-proyecto .row .region-content .content>div .galeria-en-nodo .field--item .content .field--name-field-imagen-de-la-galeria .jcarousel-wrapper .jcarousel')
+    .on('jcarousel:create jcarousel:reload', function() {
+        var element = $(this),
+            width = element.innerWidth();
+
+        if (width > 501) {
+            width = width / 2;
+        } else if (width < 500) {
+            width = width / 1;
+        }
+
+        element.jcarousel('items').css('width', width + 'px');
+    })
+    //.jcarousel({
+        // Your configurations options
+    //});
+})(jQuery);
+    
 //
 // Masonry for respositories
 //
@@ -76,144 +77,39 @@
 	
 })(jQuery);
 
-//
-//
-//
-
-//(function( $ ) {
-//  
-//  // Masonry
-//  $('.paginas-tabs .blocktabs #blocktabs-asociacion_gestion_tabs-2 .view-content').masonry({
-//    itemSelector: '.views-row',
-//    //columnWidth: '.grid-sizer',
-//    gutter: 15,
-//    //percentPosition: true,
-//    horizontalOrder: true,
-//    transitionDuration: '0.2s'
-//  });
-//  
-//  // Sidebar
-//  //$('ul[role="tablist"] li[aria-controls="blocktabs-asociacion_gestion_tabs-2"] a').on('click', function(){
-//  $('.ui-tabs-nav .ui-tabs-tab a').on('click', function(){
-//    //$('#primary').toggleClass('closed');
-//    //$('#secondary').toggleClass('closed');
-//    $('.paginas-tabs .blocktabs #blocktabs-asociacion_gestion_tabs-2 .view-content').masonry();
-//  });
-//  
-//})(jQuery);
-
 (function( $ ) {
 
-	var $container = $('.paginas-tabs .blocktabs #blocktabs-asociacion_gestion_tabs-2 .view-content');
-	$container.imagesLoaded( function () {
-		$container.masonry({
-			//columnWidth: 120,
-			itemSelector: '.views-row',
-			horizontalOrder: true,
-			percentPosition: true,
-			gutter: 50,
-			fitWidth: true,
-			//initLayout: false,
+
+		var $container = $('.paginas-tabs .blocktabs .view-content');
+		$container.imagesLoaded( function () {
+			$container.masonry({
+				//columnWidth: 120,
+				itemSelector: '.views-row',
+				horizontalOrder: true,
+				//percentPosition: true,
+				gutter: 15,
+				//fitWidth: true,
+				//initLayout: true,
+			});
 		});
-	});
-	
-	//// Sidebar
-	//  $('ul[role="tablist"] li[aria-controls="blocktabs-asociacion_gestion_tabs-2"] a').on('click', function(){
-	//    $('#primary').toggleClass('closed');
-	//    $('#secondary').toggleClass('closed');
-	//    $('.paginas-tabs .blocktabs #blocktabs-asociacion_gestion_tabs-2 .view-content').masonry();
-	//  });
-	//  
-	//  $container.masonry('reloadItems')
-	
-	//Reinitialize masonry inside each panel after the relative tab link is clicked - 
-    $('ul[role="tablist"] li[aria-controls="blocktabs-asociacion_gestion_tabs-2"] a').each(function () {
-	//$('.ui-tabs-nav .ui-tabs-tab a').each(function () {
-        var $this = $(this);
-	
-        $this.on('.paginas-tabs .blocktabs #blocktabs-asociacion_gestion_tabs-2[aria-hidden="false"]', function () {
-        
-            $container.imagesLoaded( function () {
-                $container.masonry({
-                    columnWidth: 200,
-                    itemSelector: '.views-row',
-                    gutter: 2000,
-                    fitWidth: true,
-                    horizontalOrder: true,
-                    //initLayout: true,
-                    percentPosition: true
-                });
-            });
-	
-        }); //end shown
-    });  //end each
-	
+
 })(jQuery);
 
-/*
- *
- *
- * (function( $ ) {
- * 
- * 	var $container = $( ".paginas-tabs .blocktabs #blocktabs-asociacion_gestion_tabs-2 .view-content" );
- * 	$container.masonry({
- * 	    itemSelector: ".views-row"
- * 	});
- * 	
- * 	$('ul[role="tablist"] li[aria-controls="blocktabs-asociacion_gestion_tabs-2"] a').on( 'click', function() {
- * 	    // do async to allow menu to open
- * 	    setTimeout( function() {
- * 	        $container.masonry('layout');
- * 	    });
- * 	});
- * 
- * })(jQuery);
- * 
- * (function( $ ) {
- * 
- *     var $container = $('.paginas-tabs .blocktabs #blocktabs-asociacion_gestion_tabs-2 .view-content');
- *     $container.imagesLoaded( function () {
- *         $container.masonry({
- * 			//columnWidth: '.views-row',
- * 			itemSelector: '.views-row',
- * 			horizontalOrder: true,
- * 			//fitWidth: true,
- * 			gutter: 15,
- * 			//resize: true,
- * 			//percentPosition: true
- *         });
- *     });
- *     
- *     $('ul[role="tablist"] li[aria-controls="blocktabs-asociacion_gestion_tabs-2"] a').on('click', function() {
- * 	  // do async to allow menu to open
- * 	  setTimeout( function() {
- * 	  	$('.paginas-tabs .blocktabs #blocktabs-asociacion_gestion_tabs-2 .view-content').masonry({
- * 	  		itemSelector: '.views-row',
- * 	  		horizontalOrder: true,
- * 	  		gutter: 15,
- * 	  	}, 1000);
- * 	  });
- * 	});
- *     
- *     //Reinitialize masonry inside each panel after the relative tab link is clicked - 
- *     //$('ul[role="tablist"] li[aria-controls="blocktabs-asociacion_gestion_tabs-2"] a').each(function () {
- *     //    var $this = $(this);
- * 	//
- *     //    $this.on('shown.bs.tab', function () {
- *     //    
- *     //        $container.imagesLoaded( function () {
- *     //            $container.masonry({
- *     //                //columnWidth: '.views-row',
- *     //                itemSelector: '.views-row',
- *     //                horizontalOrder: true,
- *     //                gutter: 15,
- *     //            });
- *     //        });
- * 	//
- *     //    }); //end shown
- *     //});  //end each
- *     
- * })(jQuery);
- *
- *
- */
+
+(function ($) {
+ 	$(document).ready(function() {
+ 		var $content = $(".paginas-tabs .blocktabs .view-content");
+   		$(".paginas-tabs .ui-tabs-tab a").click(function(){
+    		//alert("Clicked");
+    		$content.masonry();
+ 	 	});
+
+ 	});
+})(jQuery);
+
+(function ($) {
+	$("li.dropdown").click(function() {
+		//$("nav.navbar").toggleClass("open");
+		$("#navbar-collapse").toggleClass("open");
+	});
+})(jQuery);
